6e10924c44750cbc8a69e347ccb35c0d7a61a84a
Erik Siefken <git@posporus.de>
2020-12-29 20:24:49 +0100

    introduction

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -5,7 +5,7 @@
                 <font-awesome-icon icon="info-circle"></font-awesome-icon>
             
                 <span>
-                     Mit diesem Tool kannst du deinen Soll-Einsatzplan von <a href="http://www.adberlin.com/" target="_blank">Ambulante Dienste Berlin e.V.</a> automatisch in ein digitales Kalenderformat umwandeln und so in deinen Google Kalender, iCal, Outlook o.Ä. importieren.
+                     Mit diesem Tool kannst du deinen Soll-Einsatzplan von <a href="http://www.adberlin.com/" target="_blank">Ambulante Dienste Berlin e.V.</a> automatisch in ein digitales <a href="https://de.wikipedia.org/wiki/ICalendar" target="_blank">Kalenderformat(.ics)</a> umwandeln und so in deinen Google Kalender, Apple iCal, Outlook o.Ä. importieren.
                 </span>
             </b-alert>
             
